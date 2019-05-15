ddebaabb637f5cf504ccb98506ea7d15d6e4e59c
BCrazyDreamer <bcrazydreamer@gmail.com>
2019-05-16 00:58:06 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express                 = require("express");
+const app                     = express();
+const path                    = require('path');
+const bvalid                  = require("bvalid");
+const port                    = process.env.PORT || 3030;
+const bodyParser              = require('body-parser');
+const index                   = require('./routes/Index');
+
+var originReg = /((?:https?:\/\/)nepsho.github.io(?:\/\S*)?)|((?:https?:\/\/)bcrazydreamer.github.io(?:\/\S*)?)/;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  if(req && req.headers && bvalid.isString(req.headers["origin"])){
+    if(!originReg.test(req.headers["origin"])){
+      return res.status(404).end();
+    }
+  } else {
+    return res.status(404).end();
+  }
+  res.setHeader('Access-Control-Allow-Origin', req.headers["origin"]);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use('/', index);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.send("Not found");
+});
+
+
+app.listen(port);
---
+++ routes/Index.js
@@ -0,0 +1,34 @@
+const express               = require('express');
+const router                = express.Router();
+const bodyParser            = require('body-parser');
+const webget                = require('webget');
+const bvalid                = require("bvalid");
+
+router.post('/get_html', function(req, res)
+{   
+  var data = "please enter url firt";
+  if(bvalid.isObject(req.body) && bvalid.isString(req.body.url)){
+    webget.gethtml(req.body.url,(data)=>{
+      res.send(data);
+    })
+  }else{
+    res.send(data);
+  }
+});
+
+router.post('/get_meta', function(req, res)
+{
+  var data = "please enter url firt";
+  var option = {
+    fields: ["*"]
+  };
+  if(bvalid.isObject(req.body) && bvalid.isString(req.body.url)){
+    webget.getmeta(req.body.url,option,(data)=>{
+      res.send(data);
+    })
+  }else{
+    res.send(data);
+  }
+});
+
+module.exports = router;
